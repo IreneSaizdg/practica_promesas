8bf7490226da78f37ea4d5fadad7d9d69dbb0243
IreneSaizdg <irenesaiz@idarte.eus>
2025-05-21 15:51:39 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// VARIABLES -> EVENTOS -> FUNCIONES -> INVOCACIONES A FUNCIONES
+
+
+//VARIABLES ---------------------->
+
+
+
+
+//Array usuarios
+const userInfo =[
+    {
+        name: "user1",
+        surname: "surname1",
+        email: "user1@email.com"
+    }
+]
+
+
+
+//EVENTOS ------------------------>
+
+//FUNCIONES ---------------------->
+
