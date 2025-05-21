abe71e6ce416152e8090ed734db69c6d1cfa7d1e
IreneSaizdg <irenesaiz@idarte.eus>
2025-05-22 00:06:43 +0200

    ADD: Random Index implementation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // VARIABLES -> EVENTOS -> FUNCIONES -> INVOCACIONES A FUNCIONES
 
+
 /*Comprobación de funcionamiento
     window.alert("Funciona!");
     alert("Funciona!");*/
@@ -7,9 +8,17 @@
 
 
 
+
+
+
+
 //VARIABLES ---------------------------------------------------------------------------------->
 const buttonAskInfo = document.querySelector('#buttonAskInfo')
 const usersContainer = document.querySelector('#usersContainer')
+
+
+
+
 
 
 
@@ -23,27 +32,43 @@ const arrayUsersName =[
     { id: 5, name: "bea" },
 ]
 
+
 const arrayUsersInfo =[
     { id: 1, email: "pepe@email.com" },
     { id: 2, email: "ana@email.com" },
     { id: 5, email: "bea@email.com" },
 ]
 
+
 //Variable usuario mostrado
-const id = 1; //Dinamiza el index volviéndolo randomn. 
+const id = getRandomUserId(); //Dinamiza el index volviéndolo randomn.
+
+
 
 
 //EVENTOS ----------------------------------------------------------------------------------->
 //Evento para generar la card al hacer click en el botón información del usuario
-buttonAskInfo.addEventListener("click", () => {
-    drawUserInfo(id);// se ejecuta la función con ese id
+buttonAskInfo.addEventListener("click", () => { //recibe una función con varias instrucciones (otras funciones).
+  const id = getRandomUserId();// Ofrece un id aleatorio de entre los existentes en cada click.
+  drawUserInfo(id); // Acciona la función tomando el argumento id.
 });
 
 
 
 
 
+
+
 //FUNCIONES --------------------------------------------------------------------------------->
+//Función generar un index randomn
+function getRandomUserId() {
+  const ids = arrayUsersName.map(user => user.id);
+  const randomIndex = Math.floor(Math.random() * ids.length);
+  return ids[randomIndex];
+}
+
+
+
 
 //Funciones para conseguir nombre y email --------------------------------->
 //Función-promesa para conseguir el nombre
@@ -56,15 +81,21 @@ function getUserName(id){
 };
 
 
+
+
 //Función-promesa para conseguir el email
 function getUserEmail(id){
     const email = arrayUsersInfo.find((user) => user.id === id)?.email;
+
 
     return new Promise((resolve, reject) => {
         if (email){resolve(email)}
         else{reject(`El email del usuario con id ${id} es desconocido`)}
     })
 }
+
+
+
 
 
 
@@ -80,19 +111,25 @@ function createUserCard(){
     //Datos
     const liName = document.createElement('LI');
     liName.classList.add("userLi")
-    liName.textContent = " " 
+    liName.textContent = ""
+
 
     const liEmail = document.createElement('LI');
     liEmail.classList.add("userLi")
-    liEmail.textContent = " "
+    liEmail.textContent = ""
+
 
     //Colocación
     usersContainer.append(userCard)
     userCard.append(ulUserInfo)
     ulUserInfo.append(liName, liEmail)
 
+
     return {liName, liEmail} //Desestructuración del objeto
 }
+
+
+
 
 
 
@@ -111,6 +148,7 @@ function drawUserInfo(id) {
         console.error(`${error}`);
         });
 
+
     getUserEmail(id)
         .then((emailResult) => {
         email = emailResult;
@@ -121,9 +159,6 @@ function drawUserInfo(id) {
         console.error(`${error}`);
         });
 }
-
-
-
 
 
 
