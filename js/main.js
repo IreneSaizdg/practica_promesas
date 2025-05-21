4dcb8799d1f01d4252ee84c05fad45163e6089ea
IreneSaizdg <irenesaiz@idarte.eus>
2025-05-21 16:53:13 +0200

    ADD: basic variables and arrays

--- js/main.js
+++ js/main.js
@@ -1,23 +1,38 @@
 // VARIABLES -> EVENTOS -> FUNCIONES -> INVOCACIONES A FUNCIONES
 
+/*Comprobación de funcionamiento
+    window.alert("Funciona!");
+    alert("Funciona!");*/
+
+
+
 
 //VARIABLES ---------------------->
-
-
+const buttonAskInfo = document.querySelector('#buttonAskInfo')
+const usersContainer = document.querySelector('#usersContainer')
 
 
 //Array usuarios
-const userInfo =[
-    {
-        name: "user1",
-        surname: "surname1",
-        email: "user1@email.com"
-    }
+const arrayUsersName =[
+    { id: 1, name: "Pepe" },
+    { id: 2, name: "Ana" },
+    { id: 3, name: "Juan" },
+    { id: 4, name: "Pepi" },
+    { id: 5, name: "bea" },
+]
+
+const arrayUsersEmail =[
+    { id: 1, email: "pepe@email.com" },
+    { id: 2, email: "ana@email.com" },
+    { id: 5, email: "bea@email.com" },
 ]
 
 
 
+
 //EVENTOS ------------------------>
+
+
 
 //FUNCIONES ---------------------->
 
